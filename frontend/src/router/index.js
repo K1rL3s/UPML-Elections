a7e7566bca9a5fb3245bb85c86510889e7362976
K1rLes <kirill.lesovoi@mail.ru>
2024-02-11 23:22:35 +0500

    pwa

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -24,15 +24,12 @@ export default route(function (/* { store, ssrContext } */) {
     : createWebHashHistory;
 
   const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    routes,
-
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(
-      process.env.MODE === "ssr" ? void 0 : process.env.VUE_ROUTER_BASE
-    ),
+    history: createWebHistory(),
+    routes: routes,
+    scrollBehavior: () => ({ left: 0, top: 0 }),
   });
 
   return Router;
